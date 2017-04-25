5bd0c0527127f1c43b507cfca8ac6da22d154bf6
Rishi Amarnani <rsaamar@gmail.com>
2017-04-25 00:16:29 -0500

    Added terrain, added neighboring terrain,

--- AICharacterControl.cs
+++ AICharacterControl.cs
@@ -15,11 +15,14 @@ namespace UnityStandardAssets.Characters.ThirdPerson
         private Rigidbody rBody;
         private float increment;
         private int counter;
+        private int state; //0 == park, 1 == sidewalk, 2 == street
         private int updateFrequency;
+        private Vector3 position;
         bool moving;
 
         private void Start()
         {
+            
             // get the components on the object we need ( should not be null due to require component so no need to check )
             agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
@@ -35,34 +38,8 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             //agent.SetDestination(new Vector3(-13.0f, 5.0f, 198.0f));
         }
 
-
-        private void Update()
+        private void parkUpdate()
         {
-            //R = new System.Random();
-            //if (target != null)
-            //    agent.SetDestination(target.position);
-            //if you can still move forward, move forward
-
-
-            //character.Move2(new Vector3(1.0f, 0.0f, 0.0f), false, false, rBody);
-            //rBody.transform.Translate(new Vector3(1.0f, 0.0f, 0.0f));
-
-
-            //if (agent.remainingDistance > agent.stoppingDistance)
-            //{
-            //    character.Move(agent.desiredVelocity * 0.1f/* * increment*/, false, false);
-            //    //                character.runAnim(agent.desiredVelocity, false, false);
-            //    rBody.transform.Translate(agent.desiredVelocity * 0.1f/* * increment*/ * 1.0f);
-            //    //increment = Math.Min(2.0f, increment * 1.05f);
-            //}
-            //else
-            //{
-            //    //increment = 1.0f;
-            //    character.Move2(Vector3.zero, false, false, rBody);
-            //}
-            //if (counter % 10 == 0)
-            //{
-
             float f = UnityEngine.Random.RandomRange(0.0f, 1.0f);
             if (counter % updateFrequency == 0)
             {
@@ -79,38 +56,26 @@ namespace UnityStandardAssets.Characters.ThirdPerson
                 }
             }
             character.Move2(movement, false, false, rBody);
+        }
 
-            //    if (read < .9)
-            //    {
-            //        Console.WriteLine("read < .9");
-            //        if (moving)
-            //        {
-            //            character.Move2(movement, false, false, rBody);
-            //        }
-            //    }
-            //    else if (read < .95)
-            //    {
-            //        Console.WriteLine("read < .95");
-            //        if (moving)
-            //        {
-            //            character.Move2(Vector3.zero, false, false, rBody);
-            //            moving = false;
-            //        }
-            //        else
-            //        {
-            //            character.Move2(movement, false, false, rBody);
-            //            moving = true;
-            //        }
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("BAD");
-            //    }
-            //    //float f = R.Next();
-            //    //float f2 = R.Next();
-            //    //float f3 = R.Next();
-            //    //character.Move2(new Vector3(f, f2, f3), false, false, rBody);
-            ////}
+        private void testUpdate()
+        {
+            
+            character.Move2(new Vector3(10.0f, 0.0f, 0.0f), false, false, rBody);
+        }
+
+        private void Update()
+        {
+            position = character.transform.position;
+            if (position.x < 0)
+            {
+                parkUpdate();
+            } else 
+            {
+                testUpdate();
+            }
+
+            
             counter++;
         }
 
---
+++ NeighborScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeighborScript : MonoBehaviour {
+
+    private Terrain park;
+    private Terrain otherTerrain;
+
+	// Use this for initialization
+	void Start () {
+        park = (Terrain) GetComponent("Park");
+        otherTerrain = (Terrain) GetComponent("OtherTerrain");
+        park.SetNeighbors(new Terrain(), otherTerrain, new Terrain(), new Terrain());
+        otherTerrain.SetNeighbors(new Terrain(), new Terrain(), park, new Terrain());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
